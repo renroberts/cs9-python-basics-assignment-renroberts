adab9033f5817306667efef7491982a63175ce37
renroberts <104436980+renroberts@users.noreply.github.com>
2022-05-02 18:44:58 -0400

    Update part1.py

--- part1.py
+++ part1.py
@@ -20,5 +20,7 @@ The sum of the numbers you entered is 40
 #code starts here
 first_value = int(input("Enter a number: "))
 second_value = int(input("Enter a second number: "))
-
+third_value = int(input("Enter a third number: "))
+fourth_value = int(input("Enter a fourth number: "))
+fifth_value = int(input("Enter a fifth number: "))
 print("The sum of the numbers you entered is", first_value + second_value)
